b591c5267ac2a7173a8ffc37397566b8aa60866a
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-07-10 22:07:29 +0100

    Selectd book rendered on page as it is selected

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Javascript the good bits' },
-    { title: 'Harry Potter' },
-    { title: ' The Dark Tower' },
-    { title: 'Eloquent Ruby' },
+    { title: 'Javascript the good bits', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: ' The Dark Tower', pages: 20 },
+    { title: 'Eloquent Ruby', pages: 100 },
   ]
 }
